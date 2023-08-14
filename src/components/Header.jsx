ea9244c255e8730972ad9feac692b98faad85473
Erika <erikac.ec80@gmail.com>
2023-08-14 13:34:00 -0300

    Proyecto completo

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+
+import NuevoPresupuesto from './NuevoPresupuesto'
+import ControlPresupuesto from './ControlPresupuesto'
+
+// eslint-disable-next-line react/prop-types
+const Header = ({
+  gastos,
+  setGastos,
+  presupuesto,
+  setPresupuesto,
+  isValidPresupuesto,
+  setIsValidPresupuesto
+}) => {
+  return (
+    <header>
+        <h1>Planificador de Gastos</h1>
+
+        {isValidPresupuesto ? (
+          <ControlPresupuesto
+            gastos={gastos}
+            setGastos={setGastos}
+            presupuesto={presupuesto}
+            setPresupuesto={setPresupuesto}
+            setIsValidPresupuesto={setIsValidPresupuesto}
+          />
+        ): (
+          <NuevoPresupuesto 
+            presupuesto={presupuesto}
+            setPresupuesto={setPresupuesto}
+            setIsValidPresupuesto={setIsValidPresupuesto}
+          />
+
+        )}        
+        
+    </header>
+  )
+}
+
+export default Header
